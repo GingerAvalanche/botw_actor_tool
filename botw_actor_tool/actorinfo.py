453f890ae2ae9a0fdc897128646ef72fd25a922f
GingerAvalanche <chodness@gmail.com>
2020-10-24 20:14:33 -0700

    Add license terms to all source files

--- botw_actor_tool/actorinfo.py
+++ botw_actor_tool/actorinfo.py
@@ -1,3 +1,19 @@
+# Breath of the Wild Actor Tool, edits actor files fin LoZ:BotW
+# Copyright (C) 2020 GingerAvalanche (chodness@gmail.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # There is a loooot of bullshit in this file. It's an attempt to automate a system
 # that was probably done manually. Most of the params are able to be handled
 # automatically with a few tricks, but almost all files that get pulled from also
